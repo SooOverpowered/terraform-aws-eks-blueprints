bf704aaadb05f2f162eba00fd80de38dfa187fa8
Lê Hoàng Minh (Lucas) <lehoangminh15122000@gmail.com>
2022-08-09 14:56:53 +0700

    allow changing of desired size

--- modules/aws-eks-managed-node-groups/main.tf
+++ modules/aws-eks-managed-node-groups/main.tf
@@ -31,7 +31,7 @@ resource "aws_eks_node_group" "managed_ng" {
 
   lifecycle {
     create_before_destroy = true
-    ignore_changes        = [scaling_config[0].desired_size]
+    # ignore_changes        = [scaling_config[0].desired_size]
   }
 
   dynamic "launch_template" {
